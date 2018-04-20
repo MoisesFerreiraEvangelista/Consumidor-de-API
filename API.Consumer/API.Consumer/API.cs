2aa79c0b02e7875782a6f05fbed1cac81f665af6
84977620291@DETIC-DEV002 <mfeva2009@gmail.com>
2018-04-20 16:18:46 -0400

    Criação

---
+++ API.Consumer/API.Consumer/API.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+
+namespace API.Consumer
+{
+    public class API
+    {       
+        public static T Consumir<T>(string uri, string rota)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(uri);
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = client.GetAsync($"{rota}").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var retorno = response.Content.ReadAsStringAsync().Result;
+                    return JsonConvert.DeserializeObject<T>(retorno);
+                }
+
+                throw new Exception();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+    }
+}
---
+++ API.Consumer/ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var retorno = API.Consumer.API.Consumir<ObjetoRetorno>("https://viacep.com.br", "ws/01001000/json");
+
+            Console.WriteLine(String.Format("Logradouro: {0}", retorno.logradouro));
+            Console.WriteLine(String.Format("Bairro: {0}", retorno.bairro));
+            Console.WriteLine(String.Format("Localidade: {0}", retorno.localidade));
+            Console.WriteLine(String.Format("Estado: {0}", retorno.uf));
+
+
+            Console.ReadKey();
+        }
+    }
+    public class ObjetoRetorno
+    {
+        public string cep { get; set; }
+        public string logradouro { get; set; }
+        public string complemento { get; set; }
+        public string bairro { get; set; }
+        public string localidade { get; set; }
+        public string uf { get; set; }
+    }
+}
